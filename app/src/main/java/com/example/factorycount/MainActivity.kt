cd851c73eef9745b94b96a95cbf567b322a9e78a
sokko <angelinasokol2005@gmail.com>
2024-11-08 04:28:29 +0700

    Найдем элементы по ID

--- app/src/main/java/com/example/factorycount/MainActivity.kt
+++ app/src/main/java/com/example/factorycount/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.factorycount
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
     }
+    val viewOutput: TextView = findViewById(R.id.viewOutput)
+    val button: Button = findViewById(R.id.button)
+    val calculateN: EditText = findViewById(R.id.calculateN)
+
+
 }
